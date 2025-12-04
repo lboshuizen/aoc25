55f76648d2d32adb048d5d8d0ce3e021dccc3cc2
Leon Boshuizen <llc.boshuizen@gmail.com>
2025-12-04 10:08:31 +0100

    day 4

--- Day03.fs
+++ Day03.fs
@@ -3,7 +3,7 @@ module Day03
 let parse: string seq -> int array list = Seq.map (Seq.map a2i >> Array.ofSeq) >> List.ofSeq
 
 let pick (bn: int array) r (pos, acc) =
-    let i = seq {pos .. bn.Length - r - 1} |> Seq.maxBy (fun i -> bn[i])
+    let i = seq {pos .. bn.Length - r-1} |> Seq.maxBy (fun i -> bn[i])
     i + 1, acc * 10L + int64 bn[i]
 
 let maxJoltageN n bn = Seq.foldBack (pick bn) (Seq.init n id) (0, 0L) |> snd
---
+++ Day04.fs
@@ -0,0 +1,18 @@
+module Day04
+
+let parse: string seq -> Set<int * int> = toGrid2d >> Seq.filter (snd >> (=) '@') >> Seq.map fst >> Set.ofSeq
+
+let neighbors rolls pos = around pos |> Seq.filter (flip Set.contains rolls) |> Seq.length
+
+let accessible rolls = rolls |> Set.filter (neighbors rolls >> (>) 4)
+
+let part1 = accessible >> Set.count
+
+let part2 =
+    let step r =
+        match accessible r with
+        | acc when Set.isEmpty acc -> None
+        | acc -> Some (Set.count acc, Set.difference r acc)
+    Seq.unfold step >> Seq.sum
+
+let Solve: string seq -> _ = parse >> bothTP part1 part2
